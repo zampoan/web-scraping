4ba20321b5b03d4bc40fcc29ece47be968058716
Zheng Yao Siah <49183569+zampoan@users.noreply.github.com>
2024-02-17 12:14:37 +1100

    before looping through all web-pages

--- books_to_scrape.py
+++ books_to_scrape.py
@@ -1,25 +1,36 @@
 # ========================= 
 #        PROJECT GOAL: 
 # Webscrape and create a database of book name, price, author, descrption
+# https://www.youtube.com/watch?v=MH3641s3Roc
 # =========================
 import requests
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
-title = []
+# URL = "https://books.toscrape.com"
+# r = requests.get(URL)
+# # print(r.status_code) # returns 200 means ok
 
-URL = "https://books.toscrape.com"
-r = requests.get(URL)
-print(r.status_code) # returns 200 means ok
-
-def extract():
+def extract(num_pages):
+    title, star, price = [], [], []
+    URL = "https://books.toscrape.com/catalogue/page-2.html"
+    r = requests.get(URL)
+    # print(r.status_code) # returns 200 means ok
     soup = BeautifulSoup(r.text, 'html.parser')
     ol = soup.find('ol')
     articles = ol.find_all('article', class_='product_pod')
     for article in articles:
         image = article.find('img')
         title.append(image.attrs['alt'])
+        stars = article.find('p')
+        star.append(stars['class'][1])
+        prices = article.find('p', class_='price_color').text
+        price.append(float(prices[2:]))
+    return title, star, price
         
-    return title
-        
+def main():
+    title, star, price = extract(50)
+    print(title, star, price)
+
+main()
